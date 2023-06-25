185d63c81b02ea2086f9048b7a2fa2bb80b1a55a
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-06-25 17:05:35 +0700

    add convert date for date util; add test case

--- src/main/java/app/netlify/nmhillusion/n2mix/util/DateUtil.java
+++ src/main/java/app/netlify/nmhillusion/n2mix/util/DateUtil.java
@@ -5,15 +5,11 @@ import app.netlify.nmhillusion.n2mix.validator.StringValidator;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.ZoneId;
+import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
-
-import static app.netlify.nmhillusion.n2mix.helper.log.LogHelper.getLogger;
 
 /**
  * date: 2022-11-18
@@ -26,13 +22,13 @@ public abstract class DateUtil {
     public static final String DATEFORMAT_DDMMYYYY_h = "dd-MM-yyyy";
     public static final String YYYYMMDD_1 = "yyyy-MM-dd";
     private static final SimpleDateFormat dateFormatter = new SimpleDateFormat();
-
+    
     public static Month convertMonthFromShortNameOfMonth(String shortNameOfMonth) {
         final String trimmedMonth = StringUtil.trimWithNull(shortNameOfMonth);
         if (3 > trimmedMonth.length()) {
             return Month.JANUARY;
         }
-
+        
         switch (trimmedMonth.substring(0, 3)) {
             case "Feb":
                 return Month.FEBRUARY;
@@ -60,7 +56,7 @@ public abstract class DateUtil {
                 return Month.JANUARY;
         }
     }
-
+    
     public static boolean isLeapYear(int year) {
         if (0 == year % 4) {
             if (0 == year % 100) {
@@ -72,30 +68,30 @@ public abstract class DateUtil {
             return false;
         }
     }
-
+    
     public static LocalDate buildDateFromString(String day, String month, String year) {
         try {
             final Month month_ = DateUtil.convertMonthFromShortNameOfMonth(StringUtil.trimWithNull(month));
-
+            
             day = StringUtil.trimWithNull(day);
             int day_ = 0 == day.length() ? 1 : Integer.parseInt(day);
-
+            
             year = StringUtil.trimWithNull(year);
             final int year_ = 0 == year.length() ? 1 : Integer.parseInt(year);
-
+            
             if (!isLeapYear(year_)) {
                 if (Month.FEBRUARY.equals(month_)) {
                     day_ = Math.min(day_, 28);
                 }
             }
-
+            
             return LocalDate.of(year_, month_, day_);
         } catch (Exception ex) {
             LogHelper.getLogger(DateUtil.class).error(ex);
             return null;
         }
     }
-
+    
     public static String format(Date date, String datePattern) {
         String formattedDate = "";
         try {
@@ -108,16 +104,16 @@ public abstract class DateUtil {
         }
         return formattedDate;
     }
-
+    
     public static String format(Timestamp times, String datePattern) {
         return format(new Date(times.getTime()), datePattern);
     }
-
+    
     public static Date parse(String dateText, String datePattern) {
         Date date = null;
         try {
             if (!StringValidator.isBlank(dateText) &&
-                    !StringValidator.isBlank(datePattern)) {
+                        !StringValidator.isBlank(datePattern)) {
                 dateFormatter.applyPattern(datePattern);
                 date = dateFormatter.parse(dateText);
             }
@@ -126,7 +122,7 @@ public abstract class DateUtil {
         }
         return date;
     }
-
+    
     public static java.sql.Date parseSqlDate(Date javaDate) {
         java.sql.Date date = null;
         try {
@@ -138,22 +134,22 @@ public abstract class DateUtil {
         }
         return date;
     }
-
+    
     //duythanh 202102
     public static int getDiffDate(Date first, Date last) {
         return (int) ((last.getTime() - first.getTime()) / (1000 * 60 * 60 * 24));
     }
     //duythanh 202102
-
+    
     public static long getDiff2Date(Date beforeDate, Date afterDate) {
         return ChronoUnit.DAYS.between(beforeDate.toInstant().atZone(ZoneId.systemDefault()),
                 afterDate.toInstant().atZone(ZoneId.systemDefault()));
     }
-
+    
     public static Date truncateDate(Date date) {
         return parse(format(date, "yyyy-MM-dd"), "yyyy-MM-dd");
     }
-
+    
     /**
      * @param dateText
      * @param datePattern
@@ -169,7 +165,7 @@ public abstract class DateUtil {
         }
         return date;
     }
-
+    
     public static int getDaysOfMonth(int month, int year) {
         if (month < 1 || month > 12) { // invalid month
             return 0;
@@ -177,7 +173,7 @@ public abstract class DateUtil {
             return 31;
         } else if (2 == month) {
             if (0 != year % 100 &&
-                    0 == year % 4) {
+                        0 == year % 4) {
                 return 29;
             } else {
                 return 28;
@@ -186,11 +182,30 @@ public abstract class DateUtil {
             return 30;
         }
     }
-
+    
     public static Date addDays(Date date, int daysToAdd) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         calendar.add(Calendar.DAY_OF_YEAR, daysToAdd);
         return calendar.getTime();
+    }
+    
+    /**
+     * call to {@link DateUtil#convertToZonedDateTime(Date, ZoneId)} with zoneId is {@link ZoneId#systemDefault()}
+     */
+    public static ZonedDateTime convertToZonedDateTime(Date date_) {
+        return convertToZonedDateTime(date_, ZoneId.systemDefault());
+    }
+    
+    public static ZonedDateTime convertToZonedDateTime(Date date_, ZoneId zoneId) {
+        return ZonedDateTime.from(
+                Instant.ofEpochMilli(date_.getTime())
+                        .atZone(zoneId)
+        );
+    }
+    
+    public static Date convertZonedDateTimeToDate(ZonedDateTime zonedDateTime_) {
+        final long epochMillis = zonedDateTime_.toInstant().toEpochMilli();
+        return new java.sql.Date(epochMillis);
     }
 }
