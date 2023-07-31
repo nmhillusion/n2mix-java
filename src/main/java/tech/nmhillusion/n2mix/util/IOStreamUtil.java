cb4cfd4243db6ce7d9a4905d17e35eaf53fcec83
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-07-31 20:39:45 +0700

    check ready state for input stream

--- src/main/java/tech/nmhillusion/n2mix/util/IOStreamUtil.java
+++ src/main/java/tech/nmhillusion/n2mix/util/IOStreamUtil.java
@@ -1,8 +1,8 @@
 package tech.nmhillusion.n2mix.util;
 
 import jakarta.servlet.ServletInputStream;
-import tech.nmhillusion.n2mix.helper.log.LogHelper;
 import org.springframework.util.StreamUtils;
+import tech.nmhillusion.n2mix.helper.log.LogHelper;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,6 +12,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
+import java.util.LinkedList;
 
 /**
  * date: 2020-10-26
@@ -19,41 +20,38 @@ import java.util.Base64;
  */
 
 public abstract class IOStreamUtil {
+    
     public static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        if (!checkIfStreamIsReady(inputStream)) {
+            return "";
+        }
+        
         try (final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
-            final StringBuilder stringBuilder = new StringBuilder();
-
+            final LinkedList<String> lineList = new LinkedList<>();
+            
             String line;
             while (null != (line = bufferedReader.readLine())) {
-                stringBuilder.append(line).append("\n");
+                lineList.add(line);
             }
-
-            return stringBuilder.toString();
+            
+            return String.join("\r\n", lineList);
         }
     }
-
+    
     public static byte[] convertInputStreamToByteArray(InputStream inputStream) throws IOException {
-        if (inputStream instanceof ServletInputStream sis_) {
-            try {
-                if (!sis_.isReady()) {
-                    return new byte[0];
-                }
-            } catch (IllegalStateException ex) {
-                LogHelper.getLogger(IOStreamUtil.class).error("input stream is not ready to read. %s"
-                        .formatted(ex.getMessage()));
-                return new byte[0];
-            }
+        if (!checkIfStreamIsReady(inputStream)) {
+            return new byte[0];
         }
-
+        
         return StreamUtils.copyToByteArray(inputStream);
     }
-
+    
     public static String convertInputStreamToBase64(InputStream inputStream) throws IOException {
         final byte[] bytes = convertInputStreamToByteArray(inputStream);
         final String base64 = Base64.getEncoder().encodeToString(bytes);
         return base64;
     }
-
+    
     public static String readUtf8File(String absoluteResourcePath) {
         try {
             final byte[] byteData = Files.readAllBytes(Paths.get(absoluteResourcePath));
@@ -62,5 +60,19 @@ public abstract class IOStreamUtil {
             LogHelper.getLogger(IOStreamUtil.class).error(ex);
             return "";
         }
+    }
+    
+    private static boolean checkIfStreamIsReady(InputStream inputStream_) {
+        if (inputStream_ instanceof ServletInputStream sis_) {
+            try {
+                if (!sis_.isReady()) {
+                    return false;
+                }
+            } catch (IllegalStateException ex) {
+                LogHelper.getLogger(IOStreamUtil.class).error("input stream is not ready to read. %s".formatted(ex.getMessage()));
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/tech/nmhillusion/n2mix/util/IOStreamUtilTest.java
+++ src/test/java/tech/nmhillusion/n2mix/util/IOStreamUtilTest.java
@@ -1,61 +1,57 @@
 package tech.nmhillusion.n2mix.util;
 
-import tech.nmhillusion.n2mix.helper.log.LogHelper;
-import tech.nmhillusion.n2mix.validator.StringValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tech.nmhillusion.n2mix.validator.StringValidator;
 
 import java.io.ByteArrayInputStream;
 import java.util.Arrays;
+import java.util.Base64;
+
+import static tech.nmhillusion.n2mix.helper.log.LogHelper.getLogger;
 
 class IOStreamUtilTest {
-
+    
     @Test
-    void nullStreamTest(){
+    void nullStreamTest() {
         Assertions.assertDoesNotThrow(() -> {
             byte[] outData1 = IOStreamUtil.convertInputStreamToByteArray(null);
-
+            
             Assertions.assertEquals(0, outData1.length);
         });
     }
-
+    
     @Test
     void convertStreamToByteArray() {
-        byte[] out = new byte[0];
-        try {
-            out = IOStreamUtil.convertInputStreamToByteArray(new ByteArrayInputStream(new byte[]{
-                    1, 67, 3, 25, 65, 89, 3, 5, 78, 111, 78, 90
-            }));
-            LogHelper.getLogger(this).info(" file size: " + out.length);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        Assertions.assertNotEquals(out.length, 0);
+        Assertions.assertDoesNotThrow(() -> {
+            final byte[] expected = {1, 67, 3, 25, 65, 89, 3, 5, 78, 111, 78, 90};
+            final byte[] actual = IOStreamUtil.convertInputStreamToByteArray(new ByteArrayInputStream(expected));
+            getLogger(this).info(" file size: %s, value: %s".formatted(actual.length, Arrays.toString(actual)));
+            Assertions.assertNotEquals(0, actual.length);
+            Assertions.assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+        });
     }
-
+    
     @Test
     void convertStreamToString() {
-        String result = "";
-        try {
-            result = IOStreamUtil.convertInputStreamToString(new ByteArrayInputStream("Data is very interesting!".getBytes()));
-            LogHelper.getLogger(this).info(" result: " + result);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        Assertions.assertFalse(StringValidator.isBlank(result));
+        Assertions.assertDoesNotThrow(() -> {
+            final String expected = "Data is very interesting!";
+            final String actual = IOStreamUtil.convertInputStreamToString(new ByteArrayInputStream(expected.getBytes()));
+            getLogger(this).info(" result: " + actual);
+            Assertions.assertFalse(StringValidator.isBlank(actual));
+            Assertions.assertEquals(expected, actual);
+        });
     }
-
+    
     @Test
     void convertStreamToBase64() {
-        String result = "";
-        try {
-            result =
-                    IOStreamUtil.convertInputStreamToBase64(new ByteArrayInputStream("Data is very interesting!".getBytes()));
-            LogHelper.getLogger(this).info(" result: " + result);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        Assertions.assertFalse(StringValidator.isBlank(result));
+        Assertions.assertDoesNotThrow(() -> {
+            final String expected = "Data is very interesting!";
+            final String actualBase64 = IOStreamUtil.convertInputStreamToBase64(new ByteArrayInputStream(expected.getBytes()));
+            getLogger(this).info(" result: " + actualBase64);
+            Assertions.assertFalse(StringValidator.isBlank(actualBase64));
+            Assertions.assertEquals(expected, new String(Base64.getDecoder().decode(actualBase64)));
+        });
     }
-
+    
 }
