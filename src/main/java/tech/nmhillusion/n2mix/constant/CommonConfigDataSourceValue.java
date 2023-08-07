8298f21aefed91148b5300def25d11ed10512756
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-08-07 20:12:05 +0700

    use common dialect of oracle

--- src/main/java/tech/nmhillusion/n2mix/constant/CommonConfigDataSourceValue.java
+++ src/main/java/tech/nmhillusion/n2mix/constant/CommonConfigDataSourceValue.java
@@ -10,39 +10,39 @@ import tech.nmhillusion.n2mix.type.Stringeable;
 
 public abstract class CommonConfigDataSourceValue {
     public final static DataSourceConfig ORACLE_DATA_SOURCE_CONFIG = new DataSourceConfig()
-            .setDialectClass("org.hibernate.dialect.Oracle12cDialect")
+            .setDialectClass("org.hibernate.dialect.OracleDialect")
             .setDriverClass("oracle.jdbc.OracleDriver");
-
+    
     public final static DataSourceConfig SQL_SERVER_DATA_SOURCE_CONFIG = new DataSourceConfig()
             .setDialectClass("org.hibernate.dialect.SQLServerDialect")
             .setDriverClass("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
+    
     public final static DataSourceConfig MYSQL_DATA_SOURCE_CONFIG = new DataSourceConfig()
             .setDialectClass("org.hibernate.dialect.MySQLDialect")
             .setDriverClass("com.mysql.jdbc.Driver");
-
+    
     public final static DataSourceConfig POSTGRESQL_DATA_SOURCE_CONFIG = new DataSourceConfig()
             .setDialectClass("org.hibernate.dialect.PostgreSQLDialect")
             .setDriverClass("org.postgresql.Driver");
-
+    
     public static class DataSourceConfig extends Stringeable {
         private String driverClass;
         private String dialectClass;
-
-
+        
+        
         public String getDriverClass() {
             return driverClass;
         }
-
+        
         public DataSourceConfig setDriverClass(String driverClass) {
             this.driverClass = driverClass;
             return this;
         }
-
+        
         public String getDialectClass() {
             return dialectClass;
         }
-
+        
         public DataSourceConfig setDialectClass(String dialectClass) {
             this.dialectClass = dialectClass;
             return this;
