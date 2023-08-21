13748bcddc0385bb7b8e86533122cace34bca35e
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-08-21 21:28:49 +0700

    add testing for database

---
+++ src/test/java/tech/nmhillusion/n2mix/helper/database/ConnectionDbTest.java
@@ -0,0 +1,78 @@
+package tech.nmhillusion.n2mix.helper.database;
+
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tech.nmhillusion.n2mix.constant.CommonConfigDataSourceValue;
+import tech.nmhillusion.n2mix.helper.YamlReader;
+import tech.nmhillusion.n2mix.helper.database.config.DataSourceProperties;
+import tech.nmhillusion.n2mix.helper.database.config.DatabaseConfigHelper;
+import tech.nmhillusion.n2mix.helper.database.query.DatabaseHelper;
+import tech.nmhillusion.n2mix.helper.database.query.DatabaseWorker;
+import tech.nmhillusion.n2mix.helper.log.LogHelper;
+import tech.nmhillusion.n2mix.validator.StringValidator;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import static tech.nmhillusion.n2mix.helper.log.LogHelper.getLogger;
+
+/**
+ * date: 2023-08-21
+ * <p>
+ * created-by: nmhillusion
+ */
+
+public class ConnectionDbTest {
+    private <T> T getDatabaseConfig(String configKey, Class<T> class2Cast) throws IOException {
+        try (final InputStream dbStream = getClass().getClassLoader().getResourceAsStream("app-config/database.yml")) {
+            return new YamlReader(dbStream).getProperty(configKey, class2Cast);
+        }
+    }
+    
+    @Test
+    void testConnectDb() {
+        Assertions.assertDoesNotThrow(() -> {
+            final String githubRunID = System.getenv("github.run_id");
+            
+            if (!StringValidator.isBlank(githubRunID)) {
+                getLogger(this).warn("Ignore this test, because there is no Oracle database in github action!");
+                return;
+            } else {
+                LogHelper.getLogger(this).info("Localhost Environment ==> Running testing database");
+            }
+            
+            final CommonConfigDataSourceValue.DataSourceConfig oracleDataSourceConfig = CommonConfigDataSourceValue.ORACLE_DATA_SOURCE_CONFIG;
+            
+            final String dbUrl = getDatabaseConfig("dataSource.url", String.class);
+            final String dbUsername = getDatabaseConfig("dataSource.username", String.class);
+            final String dbPassword = getDatabaseConfig("dataSource.password", String.class);
+            
+            final DatabaseConfigHelper databaseConfigHelper = DatabaseConfigHelper.INSTANCE;
+            final DataSourceProperties dataSourceProperties = DataSourceProperties.generateFromDefaultDataSourceProperties("sample-datasource", oracleDataSourceConfig, dbUrl, dbUsername, dbPassword);
+            
+            final DataSource dataSource = databaseConfigHelper.generateDataSource(dataSourceProperties);
+            try (final SessionFactory sessionFactory = databaseConfigHelper.generateSessionFactory(dataSourceProperties)) {
+                final DatabaseHelper databaseHelper = new DatabaseHelper(dataSource, sessionFactory);
+                final DatabaseWorker dbWorker = databaseHelper.getWorker();
+                
+                dbWorker.doWork(conn -> {
+                    try (final PreparedStatement preparedStatement_ = conn.buildPreparedStatement("""
+                               select * from t_document
+                               where id in (1, 2, 3)
+                               order by id asc
+                            """)) {
+                        try (final ResultSet resultSet = preparedStatement_.executeQuery()) {
+                            while (resultSet.next()) {
+                                getLogger(this).info("document{ ID = %s, Title = %s }".formatted(resultSet.getString("id"), resultSet.getString("title")));
+                            }
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
