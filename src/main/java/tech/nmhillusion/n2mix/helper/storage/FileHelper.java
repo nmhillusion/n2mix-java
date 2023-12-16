4eac251e995b3c1658c331643fa80a15381be16a
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-12-16 14:25:10 +0700

    add some utilities for FileHelper

--- src/main/java/tech/nmhillusion/n2mix/helper/storage/FileHelper.java
+++ src/main/java/tech/nmhillusion/n2mix/helper/storage/FileHelper.java
@@ -1,6 +1,13 @@
 package tech.nmhillusion.n2mix.helper.storage;
 
+import tech.nmhillusion.n2mix.util.StringUtil;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * created by: nmhillusion
@@ -19,5 +26,41 @@ public abstract class FileHelper {
             }
         }
         return folderToDelete.delete();
+    }
+
+    public static String convertToUnixPath(String originalPath) {
+        String convertedPath = StringUtil.trimWithNull(originalPath);
+        convertedPath = convertedPath.replaceAll("\\\\", "/");
+
+        return convertedPath;
+    }
+
+    public static Optional<String> getFileExtensionFromName(String fileName) {
+        return Optional.ofNullable(fileName)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(fileName.indexOf(".") + 1))
+                ;
+    }
+
+    public static String getContentTypeOfFile(Path filePath) throws IOException {
+        final AtomicReference<String> contentType_ = new AtomicReference<>("application/octet-stream");
+
+        if (null != filePath) {
+            final Optional<String> extNameOpt_ = getFileExtensionFromName(
+                    filePath.getFileName().toString()
+            );
+
+            if (extNameOpt_.isPresent()) {
+                final String extName = extNameOpt_.get();
+
+                if ("log".equalsIgnoreCase(extName)) {
+                    contentType_.set("text/plain;charset=utf-8");
+                } else {
+                    contentType_.set(Files.probeContentType(filePath));
+                }
+            }
+        }
+
+        return contentType_.get();
     }
 }
